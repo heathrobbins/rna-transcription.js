c80448b3cfa5a4a4c11c7ab9b3a02d5f64abd23c
Heath Robbins <heathrobbins89@gmail.com>
2023-06-02 12:48:52 -0400

    Create rna-transcription.js

---
+++ rna-transcription.js
@@ -0,0 +1,14 @@
+var translations = [
+    A: 'U',
+    T: 'A',
+    C: 'G',
+    G: 'C'
+};
+function toRna(strand) {
+  strand = strand.split('');
+  return strand.map(translate).join('');
+}
+function translate(nucleotide) {
+  return translations[nucleotide];
+}
+module.exports = toRna;
